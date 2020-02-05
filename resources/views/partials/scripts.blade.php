df717cc871ec9cea53d37df3f8c382bd9ee4c886
vagrant <vagrant@homestead>
2020-02-05 11:56:00 +0000

    partials y yield de home

---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,13 @@
+<!-- Bootstrap core JavaScript -->
+<script src="{{asset('js/jquery.min.js')}}"></script>
+  <script src="{{asset('js/app.js')}}"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="{{asset('js/jquery.easing.min.js')}}"></script>
+
+  <!-- Contact Form JavaScript -->
+  <script src="{{asset('js/jqBootstrapValidation.min.js')}}"></script>
+  <script src="{{asset('js/contact_me.js')}}"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="{{asset('js/freelancer.min.js')}}"></script>
