307849db5ee3b2a030d2ab43c6747566825ae65e
vagrant <vagrant@homestead>
2020-02-14 12:13:56 +0000

    added secure assets to scripts src

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,13 +1,13 @@
 <!-- Bootstrap core JavaScript -->
-<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{secure_asset('js/jquery.min.js')}}"></script>
   <script src="{{asset('js/app.js')}}"></script>
 
   <!-- Plugin JavaScript -->
-  <script src="{{asset('js/jquery.easing.min.js')}}"></script>
+  <script src="{{secure_asset('js/jquery.easing.min.js')}}"></script>
 
   <!-- Contact Form JavaScript -->
-  <script src="{{asset('js/jqBootstrapValidation.min.js')}}"></script>
-  <script src="{{asset('js/contact_me.min.js')}}"></script>
+  <script src="{{secure_asset('js/jqBootstrapValidation.min.js')}}"></script>
+  <script src="{{secure_asset('js/contact_me.min.js')}}"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="{{asset('js/freelancer.min.js')}}"></script>
+  <script src="{{secure_asset('js/freelancer.min.js')}}"></script>
